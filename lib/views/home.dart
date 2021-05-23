f1a6a6eea16d29003f93fcf3848ef27df2c54137
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-05-23 16:36:10 -0700

    add app

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -8,8 +8,40 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+     return Scaffold( //it has appbar,body
+          appBar: AppBar(
+            title: Text("todo",style: TextStyle(fontSize: 25.0,fontFamily: "RaleWay"),),
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: (){},
+            ),
+
+            actions: [
+              IconButton(
+                icon: Icon(Icons.search),//search button
+                onPressed: (){},
+              ),
+
+              IconButton(
+                icon: Icon(Icons.more_vert),
+                onPressed: (){},
+              ),
+            ],//widget array
+
+            //set back image in background of appbar
+            flexibleSpace: Icon(
+              Icons.photo_camera,
+              size: 75.0,
+            ),
+           
+          //set bottom
+          
+
+          elevation: 3.0, //shadow of the bottom
+          backgroundColor: Colors.blueAccent[700],//set backgroud color
+           
+        ),
+
+     );
   }
 }
